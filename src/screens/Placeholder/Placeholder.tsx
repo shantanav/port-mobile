db6968235ee6ea6f8e00731935bb5a537d96ec98
Abhinav <abhinav@numberless.tech>
2023-05-08 11:45:36 +0000

    INitial setup of the app and created welcome screen

---
+++ src/screens/Placeholder/Placeholder.tsx
@@ -0,0 +1,14 @@
+/**
+ * This screen is a placeholder to test navigation
+ */
+
+import React from "react";
+import { Text } from "react-native";
+
+function Placeholder() {
+    return (
+        <Text> Placeholder screen </Text>
+    );
+}
+
+export default Placeholder;
