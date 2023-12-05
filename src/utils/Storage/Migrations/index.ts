3bd109fb58516aff78c168cddd41fc980d66a8e7
Abhinav <abhinav@mail.numberless.tech>
2023-12-05 11:00:44 +0000

    Merged PR 178: Added the foundation for migrations
    Added the foundation for migrations
    
    Related work items: #306

--- src/utils/Storage/Migrations/index.ts
+++ src/utils/Storage/Migrations/index.ts
@@ -1,10 +1,66 @@
 import {runSimpleQuery} from '../DBCalls/dbCommon';
-// Increment this counter everytime you add a migration
-const currentVersion = 1;
-console.log('Current DB version: ', currentVersion);
+// Increment this counter everytime you add a migration.
+// If this file has been modified, hopefully this counter has ticked.
+// MOST_RECENT_MIGRATION_NUMBER = 1
+
+// To run a migration, write a suitible callback and add it to the list.
+// Make sure to increment the counter above to make sure we don't do weird things.
+const migrations: [number, () => Promise<void>][] = [[1, migration1]];
 
 export default async function runMigrations() {
-  migration1();
+  await createMigrationsTable();
+  console.log('created migrations table');
+  for (let migration of migrations) {
+    console.log('trying to run migration: ', migration[0]);
+    runMigration(migration[0], migration[1]);
+  }
+}
+
+async function createMigrationsTable() {
+  await runSimpleQuery(
+    `
+    CREATE TABLE IF NOT EXISTS migrations (
+      migrationId INT PRIMARY KEY
+    );
+    `,
+    [],
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    (tx, result) => {},
+  );
+}
+
+async function runMigration(id: number, callback: () => Promise<void>) {
+  let hasRun;
+  await runSimpleQuery(
+    `
+    SELECT migrationId
+    FROM migrations
+    WHERE migrationId = ?
+    ;
+    `,
+    [id],
+
+    (tx, result) => {
+      if (result.rows.length > 0) {
+        hasRun = true;
+      } else {
+        hasRun = false;
+      }
+    },
+  );
+
+  if (!hasRun) {
+    await callback();
+    console.log('Ran migration: ', id);
+    await runSimpleQuery(
+      'INSERT INTO migrations ( migrationId ) VALUES (?) ;',
+      [id],
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      (tx, result) => {},
+    );
+  } else {
+    console.log("Didn't need to run migration: ", id);
+  }
 }
 
 /**
@@ -18,9 +74,6 @@ export default async function runMigrations() {
  *  - have an index on (messageId)
  */
 async function migration1() {
-  if (currentVersion < 1) {
-    return;
-  }
   await runSimpleQuery(
     `CREATE TABLE IF NOT EXISTS connections (
       chatId CHAR(32) PRIMARY KEY,
