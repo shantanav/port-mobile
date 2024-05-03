da78dc3eb879f68677eb897db7999cf9f061a047
Abhinav <abhinav@mail.numberless.tech>
2024-05-03 12:01:57 +0000

    Merged PR 827: Block user from using their own bundles accidentally
    Block user from using their own bundles accidentally by submitting an authorization token to the backend to assist with discovery of self-uses of bundles
    
    Steps to test:
    - Generate a link for a Port
    - Click on the link on the device that created the link. Notice that it throws an error
    - Click on the link on another device with Port installed. Notice that the connection succeeds
    
    Related work items: #666

--- src/utils/DeepLinking/APICalls.ts
+++ src/utils/DeepLinking/APICalls.ts
@@ -1,9 +1,12 @@
 import {BUNDLE_MANAGEMENT_RESOURCE} from '@configs/api';
+import {getToken} from '@utils/ServerAuth';
 import axios from 'axios';
 
 export async function getBundle(bundleId: string): Promise<string | null> {
+  const token = await getToken();
   const response = await axios.get(
     `${BUNDLE_MANAGEMENT_RESOURCE}?bundleId=${encodeURIComponent(bundleId)}`,
+    {headers: {Authorization: `${token}`}},
   );
   return response.data.bundle;
 }
