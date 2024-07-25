9ec41697a40b25e03bec76542c6acf089c7f8794
Aniruddh Balasubramaniam <aniruddh@mail.numberless.tech>
2024-07-25 03:55:11 +0000

    Merged PR 1126: minor fix of default folder migration
    fixes issue where chats in primary were not set to true for focus permission

--- src/utils/Storage/Migrations/migration006/addFocusPermissions.ts
+++ src/utils/Storage/Migrations/migration006/addFocusPermissions.ts
@@ -3,14 +3,21 @@ import {
   getFolderPermissions,
   updateFolderPermissions,
 } from '@utils/ChatFolders';
+import {updateFolderName} from '@utils/Storage/folders';
+import {updateChatPermissionsInAFolder} from '@utils/Storage/permissions';
 
 /**
  * This util sets the default(previously called Primary)
  * folder focus permission to true
  */
 export async function setFocusPermissionForDefaultFolder() {
+  await updateFolderName(defaultFolderId, 'Default');
   const permissions = await getFolderPermissions(defaultFolderId);
   await updateFolderPermissions(defaultFolderId, {
+    ...permissions,
+    focus: true,
+  });
+  await updateChatPermissionsInAFolder(defaultFolderId, {
     ...permissions,
     focus: true,
   });
