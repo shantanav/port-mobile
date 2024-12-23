081710be32985d4fbf5de577a531bef7921e1360
Shantanav Saurav <shantanav.saurav@mail.numberless.tech>
2024-12-23 11:33:03 +0000

    Merged PR 1651: Change url back to staging
    Change url back to staging

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,7 +1,7 @@
 import {DOMAIN, USE_SSL} from '@env';
 
 const useSSL = USE_SSL ? USE_SSL === 'true' : true;
-const URL = DOMAIN ? DOMAIN : 'evil-dev.numberless.tech';
+const URL = DOMAIN ? DOMAIN : 'staging.numberless.tech';
 
 /**
  * Base Endpoints
